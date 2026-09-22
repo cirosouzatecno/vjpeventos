3f7d45460dedf5e3619fe41809947dc2c1a4b755
Ciro Luis de Souza <ciro.souza3@aluno.unip.br>
2026-09-22 00:25:28 -0300

    Inspect Vercel storage variable names safely

--- api/storage-health.js
+++ api/storage-health.js
@@ -2,8 +2,12 @@ import { json, methodNotAllowed } from './_lib/http.js'
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') return methodNotAllowed(res, ['GET'])
+  const storageEnvKeys = Object.keys(process.env)
+    .filter((key) => /BLOB|STORAGE/i.test(key))
+    .sort()
   return json(res, 200, {
     databaseConfigured: Boolean(process.env.DATABASE_URL),
     blobConfigured: Boolean(process.env.BLOB_READ_WRITE_TOKEN),
+    storageEnvKeys,
   })
 }
